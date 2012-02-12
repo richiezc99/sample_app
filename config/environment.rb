767b839e618f2b9410987eac3cd0c22898bf1d1c
Richard Chen <gcguy@yahoo.com>
2012-02-11 21:35:51 -0800

    Fix location of CSS precompile

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
-
-# Precompile Blueprint CSS
-config.assets.precompile += [ "blueprint/*.css" ]
